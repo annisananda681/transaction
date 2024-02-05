654aa494dbe2a26c93e96a1ee28cb0702b45c8d3
ghhffar <ghhffar0927@gmail.com>
2024-02-05 08:25:32 +0700

    membuat controller user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    const REDIRECTHOME = "/users";
+
+    public function index()
+    {
+        if (Auth::check()) {
+            return view('users.home', [
+                'users' => User::latest('created_at')->get()
+            ]);
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+
+    public function addPage()
+    {
+        if (Auth::check()) {
+            return view('users.add');
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+
+    public function addData(Request $request)
+    {
+        if (Auth::check()) {
+            $this->validate($request, [
+                'username' => 'required',
+                'password' => 'required',
+            ]);
+
+            User::create([
+                'username' => $request->input('username'),
+                'password' => $request->input('password'),
+            ]);
+            
+            return redirect(self::REDIRECTHOME);
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+
+    public function updatePage($id)
+    {
+        if (Auth::check()) {
+            return view('users.update', [
+                'user' => User::find($id)
+            ]);
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+
+    public function updateData(Request $request)
+    {
+        if (Auth::check()) {
+            $id = $request->input('user_id');
+
+            $this->validate($request, [
+                'username' => 'required',
+                'password' => 'required',
+            ]);
+
+            $user = User::find($id);
+            $user->username = $request->input('username');
+            $user->password = $request->input('password');
+            $user->save();
+            
+            return redirect(self::REDIRECTHOME);
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+
+    public function deleteData($id)
+    {
+        if (Auth::check()) {
+            $user = User::findOrFail($id);
+            $user->delete();
+            
+            return redirect(self::REDIRECTHOME);
+        }
+   
+        return redirect('login')->withSuccess(AuthController::NOT_ALLOWED);
+    }
+}
